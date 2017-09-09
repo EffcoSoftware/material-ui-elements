562136e8007adc45909c176c3b841b1298913d0b
Tom Kostus <tom@effco.net>
2017-09-09 12:52:30 +0100

    Base layout fix

--- src/MuiForm/layouts/Base.js
+++ src/MuiForm/layouts/Base.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import FormFields from '../elements/FormFields'
+import CrudButtons from '../../CrudButtons'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 
 const FormBasic = props => {
-  const { config: { fields } } = props
+  const {
+    fields,
+    add,
+    disabled,
+    pristine,
+    submitting,
+    invalid,
+    actions,
+    label,
+    title
+  } = props
+  const formLabel = label || title || ''
 
   return (
     <div>
+      {formLabel && (
+        <Toolbar>
+          <Typography type="title">{formLabel}</Typography>
+        </Toolbar>
+      )}
       <FormFields fields={fields} {...props} />
+      <CrudButtons
+        add={add}
+        submittable={!(pristine || submitting || invalid)}
+        disabled={disabled}
+        actions={actions}
+        {...props}
+      />
     </div>
   )
 }
