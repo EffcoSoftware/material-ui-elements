76399410d535433a174254c998863fcce17b9dc9
jbinda <jbinda@effco.net>
2018-02-28 13:08:17 +0100

    move details render to library

--- src/MuiTable/elements/TableBody.js
+++ src/MuiTable/elements/TableBody.js
@@ -1,35 +1,75 @@
 import React from 'react'
+import _ from 'lodash'
 import { TableBody, TableRow, TableCell } from 'material-ui/Table'
 import MuiTableRow from './TableRow'
 
 const Body = props => {
-  const { data, fields, handleRowClick, fieldArray, colSpan } = props
+  const {
+    data,
+    fields,
+    handleRowClick,
+    fieldArray,
+    detailsRow,
+    columns
+  } = props
 
   if (!data) return null
 
   const tableData = fieldArray ? fields : data
+  let tableDataWithDetails = []
+
+  if (detailsRow) {
+    const { detailsArray, titleText, reducedProp } = detailsRow
+    tableDataWithDetails = data.reduce((r, e, i) => {
+      const dataRow = _.get(data, `[${i}]`)
+      return (
+        (!_.isEmpty(dataRow[detailsArray])
+          ? r.push(dataRow, {
+              detailRow: true,
+              value: dataRow[detailsArray].reduce((r, e, i) => {
+                return i === 0
+                  ? `${titleText}${e[reducedProp]}`
+                  : `${r}, ${e[reducedProp]}`
+              }, '')
+            })
+          : r.push(dataRow)) && r
+      )
+    }, [])
+  }
 
   return (
     <TableBody>
-      {tableData.map((row, i) => {
-        console.log(data[i])
-        return !colSpan || (data[i] && data[i].accessories.length === 0) ? (
-          <MuiTableRow
-            key={i}
-            index={i}
-            row={row}
-            hover
-            onRowClick={handleRowClick ? () => handleRowClick(row.id) : null}
-            {...props}
-          />
-        ) : (
-          <TableRow style={{ background: '#eee' }}>
-            <TableCell key={i} colSpan={12}>
-              adadd
-            </TableCell>
-          </TableRow>
-        )
-      })}
+      {!detailsRow
+        ? tableData.map((row, i) => (
+            <MuiTableRow
+              key={i}
+              index={i}
+              row={row}
+              hover
+              onRowClick={handleRowClick ? () => handleRowClick(row.id) : null}
+              {...props}
+            />
+          ))
+        : tableDataWithDetails.map((row, i) => {
+            return !row.detailRow ? (
+              <MuiTableRow
+                key={i}
+                index={i}
+                row={`${detailsRow.dataRowPath}[${i}]`}
+                hover
+                onRowClick={
+                  handleRowClick ? () => handleRowClick(row.id) : null
+                }
+                {...props}
+              />
+            ) : (
+              <TableRow style={{ background: '#eee' }}>
+                <TableCell key={i} colSpan={columns.length + 1}>
+                  {row.value}
+                </TableCell>
+              </TableRow>
+            )
+          })}
     </TableBody>
   )
 }
