5e426025b5334405401b72b7272df1d030054e2f
Tom Kostus <tom@effco.net>
2018-03-07 22:25:38 +0000

    Fix layout issues

--- src/MuiForm/layouts/Modal.js
+++ src/MuiForm/layouts/Modal.js
@@ -1,82 +1,91 @@
-import React from 'react'
-import Dialog, { DialogActions } from 'material-ui/Dialog'
+import React, { Component } from 'react'
+import Dialog, { DialogActions, DialogContent } from 'material-ui/Dialog'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
-// import IconButton from 'material-ui/IconButton'
-// import ChevronLeft from 'material-ui-icons/ChevronLeft'
-// import ChevronRight from 'material-ui-icons/ChevronRight'
-import FormTabs from '../elements/FormTabs'
+import Tabs, { Tab } from 'material-ui/Tabs'
 import FormFields from '../elements/FormFields'
 import CrudButtons from '../../CrudButtons'
 
-const FormModal = props => {
-  const {
-    add,
-    disabled,
-    pristine,
-    submitting,
-    invalid,
-    title,
-    groups,
-    fields,
-    actions,
-    open
-  } = props
+class FormModal extends Component {
+  state = { tabIndex: 0 }
 
-  return (
-    <Dialog
-      open={open}
-      // maxWidth="lg"
-      onRequestClose={actions.cancel.action || actions.cancel}
-    >
-      <AppBar
-        style={{
-          // margin: 0,
-          // paddingRight: 0,
-          position: 'relative',
-          boxShadow: 'none'
-        }}
+  render() {
+    const {
+      add,
+      disabled,
+      pristine,
+      submitting,
+      invalid,
+      title,
+      groups,
+      fields,
+      actions,
+      open
+    } = this.props
+
+    return (
+      <Dialog
+        open={open}
+        maxWidth="md"
+        fullWidth
+        onRequestClose={actions.cancel.action || actions.cancel}
       >
-        <Toolbar>
-          {/*<IconButton>
-              <ChevronLeft />
-            </IconButton>*/}
-          <div style={{ flex: 1 }} />
-          <Typography variant="title" color="inherit">
-            <div style={{ color: 'white' }}>{title || 'Details'}</div>
-          </Typography>
-          <div style={{ flex: 1 }} />
-          {/*<IconButton>
-              <ChevronRight />
-            </IconButton>*/}
-        </Toolbar>
-      </AppBar>
-      <div
-        style={{
-          width: 1000,
-          overflow: 'auto'
-        }}
-      >
-        {groups ? (
-          <FormTabs {...props} />
-        ) : (
-          <FormFields fields={fields} {...props} />
-        )}
-      </div>
-      <DialogActions>
-        {actions ? (
-          <CrudButtons
-            add={add}
-            submittable={!(pristine || submitting || invalid)}
-            disabled={disabled}
-            actions={actions}
-            {...props}
-          />
-        ) : null}
-      </DialogActions>
-    </Dialog>
-  )
+        <AppBar
+          position="static"
+          style={{
+            boxShadow: 'none'
+          }}
+        >
+          <Toolbar>
+            <div style={{ flex: 1 }} />
+            <Typography variant="title" color="inherit">
+              <div style={{ color: 'white' }}>{title || 'Details'}</div>
+            </Typography>
+            <div style={{ flex: 1 }} />
+          </Toolbar>
+          {groups && (
+            <Tabs
+              value={this.state.tabIndex}
+              onChange={(e, v) => this.setState({ tabIndex: v })}
+              indicatorColor="secondary"
+              fullWidth
+              scrollButtons="off"
+              centered
+            >
+              {groups.map((g, i) => (
+                <Tab key={i} style={{ flex: 1 }} label={g.label} />
+              ))}
+            </Tabs>
+          )}
+        </AppBar>
+        <DialogContent>
+          {groups ? (
+            <div>
+              <FormFields
+                margin={12}
+                fields={groups[this.state.tabIndex].fields}
+                {...this.props}
+              />
+            </div>
+          ) : (
+            <FormFields fields={fields} {...this.props} />
+          )}
+        </DialogContent>
+        <DialogActions>
+          {actions ? (
+            <CrudButtons
+              add={add}
+              submittable={!(pristine || submitting || invalid)}
+              disabled={disabled}
+              actions={actions}
+              {...this.props}
+            />
+          ) : null}
+        </DialogActions>
+      </Dialog>
+    )
+  }
 }
 
 export default FormModal
