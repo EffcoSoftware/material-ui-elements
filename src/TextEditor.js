9cf67616abe24c9f406bf9a87cd0d9dec5fd19c3
jbinda <jbinda@effco.net>
2018-08-27 17:19:34 +0200

    export TextEditor as standalone component, adjust to accept undefined props

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import ReactQuill from 'react-quill';
+import ReactQuill from 'react-quill'
 
-const TextEditor = ({ input, style, readOnly, theme, placeholder, defaultValue }) => (
-      <ReactQuill
-        onChange={input.onChange}
-        value={input.value}
-        style={style} 
-        readOnly={readOnly} 
-        theme={theme} 
-        placeholder={placeholder}
-        defaultValue={defaultValue}
-      />
-    )
+const TextEditor = ({
+  value,
+  input,
+  style,
+  readOnly,
+  theme,
+  placeholder,
+  defaultValue
+}) => (
+  <ReactQuill
+    onChange={input && input.onChange}
+    value={input ? input.value : value}
+    style={style}
+    readOnly={readOnly}
+    theme={theme}
+    placeholder={placeholder}
+    defaultValue={defaultValue}
+  />
+)
 
 export default TextEditor
